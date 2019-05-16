286315236371fa03bc01648c646a43ef2ffe51c7
BrennanDai <brennandai@outlook.com>
2019-05-16 15:08:37 -0700

    a

---
+++ src/Game/Main.java
@@ -0,0 +1,18 @@
+package Game;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    // yo buds
+    @Override // test123
+    public void start(Stage primaryStage){
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
